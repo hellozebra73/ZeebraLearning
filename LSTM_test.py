72dbe1a4ac11281ba560bc530647939d0c106763
hellozebra <hellozebra73@gmail.com>
2021-05-07 10:23:55 -0500

    added the seq2seq

--- LSTM_test.py
+++ LSTM_test.py
@@ -24,7 +24,6 @@ class RNNModel(nn.Module):
         tokens=torch.from_numpy(self.testArray())
         start = time.time()
         embeds = self._embed(tokens)
-        print(embeds.size(),"embeding")
         hidden,_=self.rnn(embeds)
 #         embeds = nn.utils.rnn.pack_padded_sequence(embeds, lengths, batch_first=True, enforce_sorted=False)
         linear=self._output(hidden[-1])
@@ -41,3 +40,53 @@ class RNNModel(nn.Module):
 
 modelo=RNNModel(vocab_size=1000,embed_size=400,hidden_dim=300,num_outputs=200,batch_size=30)
 modelo()
+
+
+class Decoder(nn.module):
+    def __init__(self,vocab_size,embedding_size,hidden_size,output_size,num_layers,p):
+        super(Decoder,self).__init__()
+        self.embedding=nn.Embedding(vocab_size,embedding_size)
+        self.hidden_size=hidden_size
+        self.num_layers=num_layers
+        self.rnn=nn.LSTM(embedding_size,hidden_size,num_layers,dropout=p)
+        self.fc=nn.linear(hidden_size,output_size)
+    
+    def forward(self,x,hidden,cell):
+        #x has dimmensions [1,N] where N is the batch size. The reason why we have 1 is that we process one word at a time
+        # as the next word is usually taken from either the output or of the last step or the target sentence.
+        x=x.unsqueeze(0)
+        embedding=self.embedding(x)
+        #[1,N,embedding_size]
+        outputs,(hidden,cell)=self.rnn(embedding,(hidden,cell))
+        #outputs.size()=[1,N,hidden_size]
+        #the initial hidden comes from the output of the encoder, from the on just from the previous step.
+
+        predictions=self.fc(outputs).squeeze(0)
+        #[1,N,target_vocab_size] before squeeze
+        #[N,target_vocab_size] after squeeze
+        return predictions, hidden, cell
+
+class Seq2Seq(nn.Module):
+    def__init__(self,encoder,decoder):
+        super(Seq2Seq,self).__init__()
+        self.encoder=encoder
+        self.decoder=decoder
+    def forward(self,source,target,teacher_force_ratio=0.5):
+        batch_size=source.shape[1]
+        target_len=target.shape[0]
+        #here we will save the outputs
+        outputs=torch.zeros(target_len,batch_size,target_vocab_size).to(device)
+        target_vocab_size=len(english.vocab)
+        hidden,cell=self.encoder(source)
+        #this is start token
+        x=target[0]
+        for i in range(1,len(target)):
+            output,(hidden,cell)=self.decoder(x,hidden,cell)
+            outputs[i]=output
+            best_guess=output.argmax[1]
+            random=np.rand()
+            x=target[i] if random > teacher_force_ratio else best_guess
+        return ouputs
+
+
+
